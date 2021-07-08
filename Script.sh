7cbbb1bbe5f8c2697c45f6d099d8dcd5b9231e2e
zelmorabet <zakaria.elmorabet@gmail.com>
2021-07-08 19:30:01 +0100

    script

---
+++ Script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+##########################################################
+#
+# Description : déploiement à la volée de conteneur docker
+#
+# Auteur : Zakaria
+# Date   : 07/07/2021
+#########################################################
+var="V1"
+echo $var
+! -z "$var"
